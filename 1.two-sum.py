e3de0c09966ec61d240330d2bf69ed0642f8178c
bp64 <scratchpeterson@gmail.com>
2024-07-28 04:39:27 -0500

    updated two sum solution

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -16,21 +16,14 @@ class Solution:
         # sort list
         # iterate through list once
 
-        nums_w_idxs = sorted([(i, v) for i, v in enumerate(nums)], key=lambda x: x[1])
-
-        i = 0
-        j = len(nums) - 1
-        target_not_found = True
-        while target_not_found:
-            s = target - nums_w_idxs[i][1]
-            f = nums_w_idxs[j][1]
-
-            if s == f:
-                return (nums_w_idxs[i][0], nums_w_idxs[j][0])
-            elif s > f:
-                i += 1
-            elif s < f:
-                j -= 1
+        # 3 O(n)
+        # add to hash map and check if number exists
+        seen = {}
+        for i, v in enumerate(nums):
+            s = target - v
+            if s in seen:
+                return [i, seen[s]]
+            seen[v] = i
 
 
 # @lc code=end
