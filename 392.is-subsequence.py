00f40d2e16b646b2030728032855798b61375da6
bp64 <scratchpeterson@gmail.com>
2024-08-16 00:11:04 -0500

    is-subsequence, update

--- 392.is-subsequence.py
+++ 392.is-subsequence.py
@@ -42,6 +42,7 @@
 # Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k
 # >= 10^9, and you want to check one by one to see if t has its subsequence. In
 # this scenario, how would you change your code?
+#  --> Leetcode problem 792
 #
 
 
